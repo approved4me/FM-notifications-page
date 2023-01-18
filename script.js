56f3fe9a0c90d191ccaed279d05b6c4b288567be
approved4me <approved4me@gmail.com>
2023-01-18 21:28:16 +0100

    javascript improvements

--- script.js
+++ script.js
@@ -1,6 +1,22 @@
 const notifications = document.getElementById("amount");
-const messages = document.getElementsByClassName ("single_message");
+const messages = document.getElementsByClassName("single_message");
 const newMessages = document.getElementsByClassName("new");
+const readAll = document.getElementById("read_all");
 
-notifications.innerText = "5";
-console.log(newMessages.length);
+
+notifications.innerText = newMessages.length;
+
+readAll.onclick = () => {
+    for (let i = 0; i < messages.length; i++) {
+        messages[i].classList.remove("new");
+        notifications.innerText = newMessages.length;
+    }
+}
+
+for (var i = 0; i < newMessages.length; i++) {
+    newMessages[i].onclick = function () {
+        this.classList.remove("new");
+        notifications.innerText = newMessages.length;
+    }
+}
+
