0884f763014930b6c7367fa4cc83a27d924a32d6
approved4me <approved4me@gmail.com>
2023-01-19 10:55:50 +0100

    .

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ const messages = document.getElementsByClassName("single_message");
 const newMessages = document.getElementsByClassName("new");
 const readAll = document.getElementById("read_all");
 
+const newButton = document.querySelectorAll(".new span");
+
+
 
 notifications.innerText = newMessages.length;
 
 readAll.onclick = () => {
     for (let i = 0; i < messages.length; i++) {
         messages[i].classList.remove("new");
+        newButton[i].remove();
         notifications.innerText = newMessages.length;
     }
 }
@@ -16,6 +20,7 @@ readAll.onclick = () => {
 for (var i = 0; i < newMessages.length; i++) {
     newMessages[i].onclick = function () {
         this.classList.remove("new");
+        
         notifications.innerText = newMessages.length;
     }
 }
