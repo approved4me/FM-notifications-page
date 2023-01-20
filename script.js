6f2faa991fbbea33498e34f8a3f2c87b490c0d50
approved4me <approved4me@gmail.com>
2023-01-20 08:18:27 +0100

    ready#01

--- script.js
+++ script.js
@@ -2,26 +2,35 @@ const notifications = document.getElementById("amount");
 const messages = document.getElementsByClassName("single_message");
 const newMessages = document.getElementsByClassName("new");
 const readAll = document.getElementById("read_all");
-
-const newButton = document.querySelectorAll(".new span");
-
-
+const newDott = document.querySelectorAll(".new span");
 
 notifications.innerText = newMessages.length;
+
+
 
 readAll.onclick = () => {
     for (let i = 0; i < messages.length; i++) {
         messages[i].classList.remove("new");
-        newButton[i].remove();
+        newDott.forEach(function (e) {
+            e.remove();
+        })
         notifications.innerText = newMessages.length;
     }
 }
 
+
 for (var i = 0; i < newMessages.length; i++) {
     newMessages[i].onclick = function () {
         this.classList.remove("new");
-        
+
+        // newDott.forEach.remove;
+
+        newDott.forEach(function (e) {
+            e.remove();
+        })
+
         notifications.innerText = newMessages.length;
     }
+
 }
 
